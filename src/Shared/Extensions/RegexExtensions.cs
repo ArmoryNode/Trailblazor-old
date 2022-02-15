1224be6c93014223bf2faaea1411f7279951b05f
Andy <andy@shapemydistrict.com>
2022-02-14 23:29:40 -0800

    Added resize handlers to window component

---
+++ src/Shared/Extensions/RegexExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Trailblazor.Shared.Extensions
+{
+    public static class RegexHelpers
+    {
+        public const string CSSLengthPattern = @"^(\d*\.?\d+)+(px|ex|em|rem|%|vh|vw|in|cm|mm|pt|pc)$";
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException"/> if the provided text does not satisfy regex pattern.
+        /// </summary>
+        /// <param name="regex"></param>
+        /// <param name="textToMatch"></param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="RegexParseException"></exception>
+        /// <exception cref="RegexMatchTimeoutException"></exception>
+        public static void ThrowIfNoMatch(string regex, string textToMatch)
+        {
+            if (!Regex.IsMatch(textToMatch, regex))
+                throw new ArgumentException("Provided text does not satisfy the regex pattern.");
+        }
+    }
+}
