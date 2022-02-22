c120505c1f95e67b987949fc90eedcbaae8e3479
Andy <andy@shapemydistrict.com>
2022-02-21 21:41:15 -0800

    Added ability to resize window component

--- src/Client/Shared/Components/Window.razor.js
+++ src/Client/Shared/Components/Window.razor.js
@@ -1,4 +1,4 @@
-export function topbarMouseDown(event, windowElement, containerElement) {
+export function topbarMouseDown(event, windowElement, containerElement = null) {
     if (typeof windowElement.style === "undefined")
         throw "Parameter `windowElement` is not an HTML element";
     containerElement = containerElement !== null && containerElement !== void 0 ? containerElement : document.body;
@@ -36,4 +36,63 @@ export function topbarMouseDown(event, windowElement, containerElement) {
     };
     document.addEventListener('mousemove', dragStart);
     document.addEventListener('mouseup', dragEnd);
+}
+export function resizeSideHandleMouseDown(event, windowElement, side, containerElement = null, minWidth = 300, minHeight = 300) {
+    if (typeof windowElement.style === "undefined")
+        throw "Parameter `windowElement` is not an HTML element";
+    containerElement = containerElement !== null && containerElement !== void 0 ? containerElement : document.body;
+    const windowBoundingClientRect = windowElement.getBoundingClientRect();
+    const containerBoundingClientRect = containerElement.getBoundingClientRect();
+    let initialWidth = windowBoundingClientRect.width;
+    let initialHeight = windowBoundingClientRect.height;
+    let initialX = windowBoundingClientRect.left;
+    let initialY = windowBoundingClientRect.top;
+    let initialMouseX = event.clientX;
+    let initialMouseY = event.clientY;
+    const resizeWindow = (e) => {
+        const width = initialWidth + (event.pageX - initialMouseX);
+        const height = initialHeight + (event.pageY - initialMouseY);
+        const handleX = e.clientX - initialMouseX;
+        const handleY = e.clientY - initialMouseY;
+        const containerWidth = containerBoundingClientRect.right;
+        const containerHeight = containerBoundingClientRect.bottom;
+        switch (side) {
+            case 'left':
+                if (e.pageX > 0 && width > minWidth + handleX) {
+                    windowElement.style.width = `${width - handleX}px`;
+                    windowElement.style.left = `${initialX + (e.pageX - initialMouseX)}px`;
+                }
+                break;
+            case 'right':
+                if (e.pageX < containerWidth && width > minWidth - handleX) {
+                    if (handleX > containerWidth) {
+                        windowElement.style.width = `${width}px`;
+                        break;
+                    }
+                    windowElement.style.width = `${width + handleX}px`;
+                }
+                break;
+            case 'top':
+                if (e.pageY > 0 && height > minHeight + handleY) {
+                    windowElement.style.height = `${height - handleY}px`;
+                    windowElement.style.top = `${initialY + (e.pageY - initialMouseY)}px`;
+                }
+                break;
+            case 'bottom':
+                if (e.pageY < containerHeight && height > minHeight - handleY) {
+                    if (handleY > containerHeight) {
+                        windowElement.style.height = `${height}px`;
+                        break;
+                    }
+                    windowElement.style.height = `${height + handleY}px`;
+                }
+                break;
+        }
+    };
+    const stopResize = (e) => {
+        document.removeEventListener('mouseup', stopResize);
+        document.removeEventListener('mousemove', resizeWindow);
+    };
+    document.addEventListener('mousemove', resizeWindow);
+    document.addEventListener('mouseup', stopResize);
 }
--- src/Client/Shared/Components/Window.razor.ts
+++ src/Client/Shared/Components/Window.razor.ts
@@ -1,4 +1,4 @@
-﻿export function topbarMouseDown(event: MouseEvent, windowElement: HTMLElement, containerElement?: HTMLElement): void {
+﻿export function topbarMouseDown(event: MouseEvent, windowElement: HTMLElement, containerElement: HTMLElement = null): void {
     if (typeof windowElement.style === "undefined")
         throw "Parameter `windowElement` is not an HTML element";
 
@@ -53,4 +53,77 @@
 
     document.addEventListener('mousemove', dragStart);
     document.addEventListener('mouseup', dragEnd);
+}
+
+export function resizeSideHandleMouseDown(event: MouseEvent, windowElement: HTMLElement, side: 'left' | 'right' | 'top' | 'bottom', containerElement: HTMLElement = null, minWidth: number = 300, minHeight: number = 300) {
+    if (typeof windowElement.style === "undefined")
+        throw "Parameter `windowElement` is not an HTML element";
+
+    containerElement = containerElement ?? document.body;
+
+    const windowBoundingClientRect = windowElement.getBoundingClientRect();
+    const containerBoundingClientRect = containerElement.getBoundingClientRect();
+
+    let initialWidth = windowBoundingClientRect.width;
+    let initialHeight = windowBoundingClientRect.height;
+
+    let initialX = windowBoundingClientRect.left;
+    let initialY = windowBoundingClientRect.top;
+
+    let initialMouseX = event.clientX;
+    let initialMouseY = event.clientY;
+
+    const resizeWindow = (e: MouseEvent): void => {
+        const width = initialWidth + (event.pageX - initialMouseX);
+        const height = initialHeight + (event.pageY - initialMouseY);
+
+        const handleX = e.clientX - initialMouseX;
+        const handleY = e.clientY - initialMouseY;
+
+        const containerWidth = containerBoundingClientRect.right;
+        const containerHeight = containerBoundingClientRect.bottom;
+
+        switch (side) {
+            case 'left':
+                if (e.pageX > 0 && width > minWidth + handleX) {
+                    windowElement.style.width = `${width - handleX}px`;
+                    windowElement.style.left = `${initialX + (e.pageX - initialMouseX)}px`;
+                }
+                break;
+            case 'right':
+                if (e.pageX < containerWidth && width > minWidth - handleX) {
+                    // confine in container/body
+                    if (handleX > containerWidth) {
+                        windowElement.style.width = `${width}px`;
+                        break;
+                    }
+                    windowElement.style.width = `${width + handleX}px`;
+                }
+                break;
+            case 'top':
+                if (e.pageY > 0 && height > minHeight + handleY) {
+                    windowElement.style.height = `${height - handleY}px`;
+                    windowElement.style.top = `${initialY + (e.pageY - initialMouseY)}px`;
+                }
+                break;
+            case 'bottom':
+                if (e.pageY < containerHeight && height > minHeight - handleY) {
+                    // confine in container/body
+                    if (handleY > containerHeight) {
+                        windowElement.style.height = `${height}px`;
+                        break;
+                    }
+                    windowElement.style.height = `${height + handleY}px`;
+                }
+                break;
+        }
+    };
+
+    const stopResize = (e: MouseEvent): void => {
+        document.removeEventListener('mouseup', stopResize);
+        document.removeEventListener('mousemove', resizeWindow);
+    };
+
+    document.addEventListener('mousemove', resizeWindow);
+    document.addEventListener('mouseup', stopResize);
 }
